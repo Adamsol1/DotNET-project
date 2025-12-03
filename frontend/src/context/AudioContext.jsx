7b413ec722c93d2bbb1851c917599025783f1b68
Ahmed <ahmed@capusx.no>
2025-12-03 18:12:47 +0100

    👍
    👍

--- frontend/src/context/AudioContext.jsx
+++ frontend/src/context/AudioContext.jsx
@@ -1,4 +1,4 @@
-﻿import React, { createContext, useContext, useRef, useState } from 'react';
+import React, { createContext, useContext, useRef, useState } from 'react';
 
 const AudioContext = createContext();
 
